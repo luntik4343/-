e3e21cc5c53c2398095eeebacef65393ef0b0b4f
Лунтек <69987863+luntik4343@users.noreply.github.com>
2020-08-21 13:50:20 +0300

    Общая сборка моих баток
    Удачи хахаахах

---
+++ Test_To_dIE_hahahah_sorry_this_is_joke.bat
@@ -0,0 +1,54 @@
+@echo off
+echo HeLlO
+echo ItS  ViRus mRmApOop
+echo click any button for activate
+pause
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+pause 
+echo DIE
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+echo D.I.E HAHAHAHHAHA
+pause
+start notepad.exe
+pause
